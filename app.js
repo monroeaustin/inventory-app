5893243fc39f10657474e72b03850c1914ec8dbd
Monroe Austin <monroeraustin2@yahoo.com>
2025-07-25 00:17:44 -0700

    Fix: Fixed CSS Load:

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require("node:path");
+
 const productRoutes = require('./controller/mainController')
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })).use('/', productRoutes.showHomePage);
+
 
 app.listen(3000,() => {
 
