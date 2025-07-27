b7a69c7cb5d918774e4b63255011f3890adad571
Monroe Austin <monroeraustin2@yahoo.com>
2025-07-27 06:27:44 -0700

    Revised

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,11 @@
+const { Pool } = require("pg");
 require("dotenv").config();
 
-const { Pool } = require("pg");
 
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
+
+
 module.exports = new Pool({
-  host: "postgres.railway.internal", // or wherever the db is hosted
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
