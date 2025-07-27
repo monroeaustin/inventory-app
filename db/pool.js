9b9596fd9b409d41282da0e67efe163b8100cbe4
Monroe Austin <monroeraustin2@yahoo.com>
2025-07-27 06:13:05 -0700

    Integrate DB to Railway

--- db/pool.js
+++ db/pool.js
@@ -6,7 +6,7 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
+  host: "postgres.railway.internal", // or wherever the db is hosted
   user: process.env.USER,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
